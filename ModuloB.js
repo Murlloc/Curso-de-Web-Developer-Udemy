34a68eafa42d490cafef1e18c47d11f8fa04a9fb
Murlloc <39132140+Murlloc@users.noreply.github.com>
2019-07-22 16:56:38 -0300

    Delete ModuloB.js

--- ModuloB.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    bomDia: 'bom dia',
-    DeclaracaoDeRenda(num1,num2){
-       return DeclaracaoDeRenda(num1,num2)
-    }
-}
-function DeclaracaoDeRenda(num1,num2){
-    return num1 + num2
-}
